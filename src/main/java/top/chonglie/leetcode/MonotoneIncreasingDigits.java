3ba0a7d2fcd290e9399be595d419b71c148043bd
luochonglie <luochonglie@163.com>
2020-12-18 00:43:04 +0800

    换成速度最佳的算法

--- src/main/java/top/chonglie/leetcode/MonotoneIncreasingDigits.java
+++ src/main/java/top/chonglie/leetcode/MonotoneIncreasingDigits.java
@@ -1,11 +1,29 @@
 package top.chonglie.leetcode;
 
+import java.util.Arrays;
+
 public class MonotoneIncreasingDigits {
     public int monotoneIncreasingDigits(int N) {
-        int result = N;
+        int result = 0;
+        int digitMax = 10;
+        int digit = 1;
 
+        while (N > 0){
+            int t = N % 10;
+            if(t <= digitMax){
+                result += t * digit;
+                digitMax = t;
+            }else {
+                result = t * digit - 1;
+                digitMax = t - 1;
+            }
+
+            N /= 10;
+            digit *= 10;
+        }
 
 
         return result;
+
     }
 }
---
+++ src/test/java/top/chonglie/leetcode/MonotoneIncreasingDigitsTest.java
@@ -0,0 +1,22 @@
+package top.chonglie.leetcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MonotoneIncreasingDigitsTest {
+
+    MonotoneIncreasingDigits solution = new MonotoneIncreasingDigits();
+    @Test
+    public void monotoneIncreasingDigits() {
+        Assert.assertEquals(123, solution.monotoneIncreasingDigits(123));
+
+        Assert.assertEquals(9, solution.monotoneIncreasingDigits(10));
+
+
+        Assert.assertEquals(8999, solution.monotoneIncreasingDigits(9998));
+
+        Assert.assertEquals(999, solution.monotoneIncreasingDigits(1101));
+    }
+}
