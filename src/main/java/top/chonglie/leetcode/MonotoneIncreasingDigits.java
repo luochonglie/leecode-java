fc3cc29d319532013825d86d012a82ce379dad58
luochonglie <luochonglie@163.com>
2020-12-17 23:06:21 +0800

    初始化

---
+++ src/main/java/top/chonglie/leetcode/MonotoneIncreasingDigits.java
@@ -0,0 +1,11 @@
+package top.chonglie.leetcode;
+
+public class MonotoneIncreasingDigits {
+    public int monotoneIncreasingDigits(int N) {
+        int result = N;
+
+
+
+        return result;
+    }
+}
