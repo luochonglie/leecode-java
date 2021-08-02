dc3573ea81c560449a90512e15806b991d977db3
luochonglie <luochonglie@163.com>
2021-08-03 01:15:20 +0800

    增加注解和题目URL

--- src/main/java/top/chonglie/leetcode/MonotoneIncreasingDigits.java
+++ src/main/java/top/chonglie/leetcode/MonotoneIncreasingDigits.java
@@ -1,8 +1,33 @@
 package top.chonglie.leetcode;
 
 /**
+ * 单调递增的数字
+ * <p>给定一个非负整数<b>N</b>，找出小于或等于<b>N</b>的最大的整数，同时这个整数需要满足其各个位数上的数字是单调递增。</p>
+ * <p>（当且仅当每个相邻位数上的数字<b>x</b>和<b>y</b>满足<b>x</b> <= <b>y</b>时，我们称这个整数是单调递增的。）</p>
  *
- * @see <a href=""></a>
+ * <p>
+ * 示例 1:
+ * <blockquote>
+ * <pre>
+ * 输入: N = 10
+ * 输出: 9
+ * </pre>
+ * </blockquote>
+ * </p>
+ *
+ * <p>
+ * 示例 2:
+ * <blockquote>
+ * <pre>
+ * 输入: N = 1234
+ * 输出: 1234
+ * </pre>
+ * </blockquote>
+ * </p>
+ *
+ * <p>说明: N 是在 [0, 10^9] 范围内的一个整数。</p>
+ *
+ * @see <a href="https://leetcode-cn.com/problems/monotone-increasing-digits/">单调递增的数字</a>
  */
 public class MonotoneIncreasingDigits {
     public int monotoneIncreasingDigits(int N) {
--- src/test/java/top/chonglie/leetcode/MonotoneIncreasingDigitsTest.java
+++ src/test/java/top/chonglie/leetcode/MonotoneIncreasingDigitsTest.java
@@ -8,12 +8,12 @@ import static org.junit.Assert.*;
 public class MonotoneIncreasingDigitsTest {
 
     MonotoneIncreasingDigits solution = new MonotoneIncreasingDigits();
+
     @Test
     public void monotoneIncreasingDigits() {
         Assert.assertEquals(123, solution.monotoneIncreasingDigits(123));
 
         Assert.assertEquals(9, solution.monotoneIncreasingDigits(10));
-
 
         Assert.assertEquals(8999, solution.monotoneIncreasingDigits(9998));
 
