9f157463519b9632d61162d9cfda37aff21123c4
luochonglie <luochonglie@163.com>
2021-08-03 00:59:24 +0800

    增加注解和题目URL

--- src/main/java/top/chonglie/leetcode/AddTwoNumbers.java
+++ src/main/java/top/chonglie/leetcode/AddTwoNumbers.java
@@ -4,6 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * 两数相加
+ * <p>给你两个 <b>非空</b> 的链表，表示两个非负的整数。它们每位数字都是按照 <b>逆序</b> 的方式存储的，并且每个节点只能存储 <b>一位</b> 数字。</p>
+ * <p>请你将两个数相加，并以相同形式返回一个表示和的链表。</p>
+ * <p>你可以假设除了数字 0 之外，这两个数都不会以 0 开头。</p>
+ * <p></p>
+ *
+ * <p>
+ * 示例 1：
+ * <blockquote>
+ * <pre>
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[7,0,8]
+ * 解释：342 + 465 = 807.
+ * <image src="https://assets.leetcode-cn.com/aliyun-lc-upload/uploads/2021/01/02/addtwonumber1.jpg"></image>
+ * </pre>
+ * </blockquote>
+ * </p>
+ *
+ * <p>
+ * 提示：
+ * <blockquote>
+ * <pre>
+ * 每个链表中的节点数在范围 [1, 100] 内
+ * 0 <= Node.val <= 9
+ * 题目数据保证列表表示的数字不含前导零
+ * </pre>
+ * </blockquote>
+ * </p>
+ *
+ * @see <a href="https://leetcode-cn.com/problems/add-two-numbers/">两数相加</a>
+ */
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         if (Objects.isNull(l1) || Objects.isNull(l2)) {
--- src/main/java/top/chonglie/leetcode/MonotoneIncreasingDigits.java
+++ src/main/java/top/chonglie/leetcode/MonotoneIncreasingDigits.java
@@ -1,23 +1,27 @@
 package top.chonglie.leetcode;
 
+/**
+ *
+ * @see <a href=""></a>
+ */
 public class MonotoneIncreasingDigits {
     public int monotoneIncreasingDigits(int N) {
         int result = 0;
         int max = 10;
         int multiple = 1;
 
-        while (N > 0){
+        while (N > 0) {
             int digit = N % 10;
 
             N /= 10;
 
             int high = N % 10;
 
-            if(high > digit){
+            if (high > digit) {
                 result = multiple * 10 - 1;
                 N -= 1;
-            }else {
-                result += digit * multiple ;
+            } else {
+                result += digit * multiple;
             }
 
             multiple *= 10;
