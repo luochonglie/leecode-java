3aeb508eb8014c290d00be0aa9e0bbe16825152b
luochonglie <luochonglie>
2022-01-29 10:40:31 +0800

    1.使用ParameterizedTest来参数化入参 2.使用CsvSource初始化参数列表

--- src/test/java/top/chonglie/leetcode/ReverseIntegerTest.java
+++ src/test/java/top/chonglie/leetcode/ReverseIntegerTest.java
@@ -3,6 +3,8 @@ package top.chonglie.leetcode;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  * ReverseInteger Tester.
@@ -11,60 +13,23 @@ import org.junit.jupiter.api.Test;
  * @version 1.0
  * @since <pre>01/27/2022</pre>
  */
-public class ReverseIntegerTest {
+class ReverseIntegerTest {
 
     ReverseInteger solution = new ReverseInteger();
 
     /**
      * Method: reverse(int x)
      */
-    @Test
-    public void testReversePositive() {
-        int param = 123;
-        int expect = 321;
-
-        Assertions.assertEquals(expect, solution.reverse(param));
+    @ParameterizedTest
+    @CsvSource({
+            "123, 321"
+            , "12300, 321"
+            , "-12, -21"
+            , "-10, -1"
+            , "2147483647, 0"
+            , "-2147483412, -2143847412"
+    })
+    void testReverse(int parameter, int expected) {
+        Assertions.assertEquals(expected, solution.reverse(parameter));
     }
-
-    @Test
-    public void testReversePositiveEndWithZero() {
-        int param = 12300;
-        int expect = 321;
-
-        Assertions.assertEquals(expect, solution.reverse(param));
-    }
-
-
-    @Test
-    public void testReverseBiggerThanMaxInt() {
-        int param = 2147483647;
-        int expect = 0;
-
-        Assertions.assertEquals(expect, solution.reverse(param));
-    }
-
-    @Test
-    public void testReverseNegative() {
-        int param = -12;
-        int expect = -21;
-
-        Assertions.assertEquals(expect, solution.reverse(param));
-    }
-
-    @Test
-    public void testReverseNegativeEndWithZero() {
-        int param = -10;
-        int expect = -1;
-
-        Assertions.assertEquals(expect, solution.reverse(param));
-    }
-
-    @Test
-    public void testReverse_3() {
-        int param = -2147483412;
-        int expect = -2143847412;
-
-        Assertions.assertEquals(expect, solution.reverse(param));
-    }
-
 } 
