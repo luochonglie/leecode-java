2fd359a845442feec36605a2813c12451a8d132c
luochonglie <luochonglie>
2022-01-30 23:47:54 +0800

    1.将字符串转换为整数数组

--- src/test/java/top/chonglie/leetcode/TwoSumTest.java
+++ src/test/java/top/chonglie/leetcode/TwoSumTest.java
@@ -2,7 +2,10 @@ package top.chonglie.leetcode;
 
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.stream.Stream;
 
 /**
  * TwoSum Tester.
@@ -11,51 +14,27 @@ import org.junit.jupiter.api.Test;
  * @version 1.0
  * @since <pre>01/27/2022</pre>
  */
-public class TwoSumTest {
+class TwoSumTest {
 
     TwoSum solution = new TwoSum();
 
     /**
      * Method: twoSum(int[] nums, int target)
      */
-    @Test
-    public void testTwoSum_1() throws Exception {
-        int[] nums = new int[]{2, 7, 11, 15};
-        int target = 9;
-
-        int[] expects = new int[]{0, 1};
-
-        Assertions.assertArrayEquals(expects, solution.twoSum(nums, target));
+    @ParameterizedTest
+    @CsvSource({
+            "'[2, 7, 11, 15]', 9,  '[0, 1]'"
+            , "'[3, 2, 4]', 6, '[1, 2]'"
+            , "'[3, 3]', 6 , '[0, 1]'"
+            , "'[2, 5, 5, 11]', 10, '[1, 2]'"
+    })
+    void testTwoSum(String nums, int target, String expect) throws Exception {
+        Assertions.assertArrayEquals(string2IntArray(expect), solution.twoSum(string2IntArray(nums), target));
     }
 
-    @Test
-    public void testTwoSum_2() throws Exception {
-        int[] nums = new int[]{3, 2, 4};
-        int target = 6;
-
-        int[] expects = new int[]{1, 2};
-
-        Assertions.assertArrayEquals(expects, solution.twoSum(nums, target));
+    private int[] string2IntArray(String input) {
+        return Stream.of(input.substring(1, input.length() - 1).split(",\\s+")).mapToInt(Integer::parseInt).toArray();
     }
 
-    @Test
-    public void testTwoSum_3() throws Exception {
-        int[] nums = new int[]{3, 3};
-        int target = 6;
-
-        int[] expects = new int[]{0, 1};
-
-        Assertions.assertArrayEquals(expects, solution.twoSum(nums, target));
-    }
-
-    @Test
-    public void testTwoSum_4() throws Exception {
-        int[] nums = new int[]{2,5,5,11};
-        int target = 10;
-
-        int[] expects = new int[]{1, 2};
-
-        Assertions.assertArrayEquals(expects, solution.twoSum(nums, target));
-    }
 
 }
