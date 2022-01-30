d52666cc426e3bc8447794603928a02d20734b2d
luochonglie <luochonglie>
2022-01-31 00:23:09 +0800

    1.将复杂参数的输入方式改为源自参数方法

--- src/test/java/top/chonglie/leetcode/TwoSumTest.java
+++ src/test/java/top/chonglie/leetcode/TwoSumTest.java
@@ -3,7 +3,10 @@ package top.chonglie.leetcode;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
@@ -18,23 +21,22 @@ class TwoSumTest {
 
     TwoSum solution = new TwoSum();
 
+    static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1})
+                , Arguments.of(new int[]{3, 2, 4}, 6, new int[]{1, 2})
+                , Arguments.of(new int[]{3, 3}, 6, new int[]{0, 1})
+                , Arguments.of(new int[]{2, 5, 5, 11}, 10, new int[]{1, 2})
+        );
+    }
+
     /**
      * Method: twoSum(int[] nums, int target)
      */
     @ParameterizedTest
-    @CsvSource({
-            "'[2, 7, 11, 15]', 9,  '[0, 1]'"
-            , "'[3, 2, 4]', 6, '[1, 2]'"
-            , "'[3, 3]', 6 , '[0, 1]'"
-            , "'[2, 5, 5, 11]', 10, '[1, 2]'"
-    })
-    void testTwoSum(String nums, int target, String expect) throws Exception {
-        Assertions.assertArrayEquals(string2IntArray(expect), solution.twoSum(string2IntArray(nums), target));
+    @MethodSource("arguments")
+    void testTwoSum(int[] nums, int target, int[] expect) {
+        Assertions.assertArrayEquals(expect, solution.twoSum(nums, target));
     }
-
-    private int[] string2IntArray(String input) {
-        return Stream.of(input.substring(1, input.length() - 1).split(",\\s+")).mapToInt(Integer::parseInt).toArray();
-    }
-
 
 }
