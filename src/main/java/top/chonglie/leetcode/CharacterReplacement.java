0b6f2a783e181599f11c7bd9316599ff4b813d92
luochonglie <luochonglie>
2022-02-26 23:12:47 +0800

    提交题目

---
+++ src/main/java/top/chonglie/leetcode/CharacterReplacement.java
@@ -0,0 +1,13 @@
+package top.chonglie.leetcode;
+
+public class CharacterReplacement {
+
+    public int characterReplacement(String s, int k) {
+        // 1.查找最长重复字符串的位置
+
+        // 2.修改左边或右边的字母
+        return 0;
+    }
+
+
+}
---
+++ src/test/java/top/chonglie/leetcode/CharacterReplacementTest.java
@@ -0,0 +1,20 @@
+package top.chonglie.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class CharacterReplacementTest {
+    CharacterReplacement characterReplacement = new CharacterReplacement();
+
+    @ParameterizedTest
+    @CsvSource({
+            "'', 1, 0"
+            ,"ABAB, 2, 4"
+            , "AABABBA, 1, 4"
+    })
+    void testCharacterReplacement(String s, int k, int expected) {
+        Assertions.assertEquals(expected, characterReplacement.characterReplacement(s, k));
+    }
+}
