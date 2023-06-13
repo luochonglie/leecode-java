127b863944931a65a873ba77bbf1ddcfe831c7e0
luochonglie <luochonglie@163.com>
2023-06-13 23:35:40 +0800

    提交到云端保存

--- src/main/java/top/chonglie/RenameTool.java
+++ src/main/java/top/chonglie/RenameTool.java
@@ -1,8 +1,6 @@
 package top.chonglie;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import sun.nio.ch.IOUtil;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,6 +24,24 @@ public class RenameTool {
             System.out.println(Arrays.toString(file.getName().split(separator)));
             if(file.getName().split(separator)[0].length() < 3){
                 String newFileName = "0" + file.getName();
+
+                System.out.println("From: " + file.getName() + ", to: " + newFileName);
+                FileUtils.moveFile(file, FileUtils.getFile(new File(path), newFileName));
+            }
+        }
+    }
+
+    public static void changeFileName(String path, String separator, String prefix) throws IOException {
+        //获取指定目录下的所有文件
+        Collection<File> files = FileUtils.listFiles(new File(path), new String[]{"mp4"}, false);
+
+        for (File file : files) {
+            System.out.println(Arrays.toString(file.getName().split(separator)));
+            //文件名前面增加指定个数的0
+            file.getName().split(separator)[0].length();
+            
+            if(file.getName().split(separator)[0].length() < 3){
+                String newFileName = prefix + file.getName();
 
                 System.out.println("From: " + file.getName() + ", to: " + newFileName);
                 FileUtils.moveFile(file, FileUtils.getFile(new File(path), newFileName));
--- src/main/java/top/chonglie/leetcode/CharacterReplacement.java
+++ src/main/java/top/chonglie/leetcode/CharacterReplacement.java
@@ -2,6 +2,30 @@ package top.chonglie.leetcode;
 
 public class CharacterReplacement {
 
+    /**
+     * 424. 替换后的最长重复字符
+     * <p>
+     * 给你一个仅由大写英文字母组成的字符串，你可以将任意位置上的字符替换成另外的字符，总共可最多替换 k 次。在执行上述操作后，找到包含重复字母的最长子串的长度。
+     * <p>
+     * 注意：字符串长度 和 k 不会超过 104。
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：s = "ABAB", k = 2
+     * 输出：4
+     * 解释：用两个'A'替换为两个'B',反之亦然。
+     * 示例 2：
+     * <p>
+     * 输入：s = "AABABBA", k = 1
+     * 输出：4
+     * 解释：
+     * 将中间的一个'A'替换为'B',字符串变为 "AABBBBA"。
+     * 子串 "BBBB" 有最长重复字母, 答案为 4。
+     *
+     * @param s 字符串
+     * @param k 替换次数
+     * @return 最长重复字符
+     */
     public int characterReplacement(String s, int k) {
         // 取得字符串长度（能提速1ms）
         int sLength = s.length();
